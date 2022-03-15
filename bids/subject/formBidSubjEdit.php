820cce0b565bcf3c4f1d9f6dd2e16eb700613d97
Konung1973 <e.ryabov@integral.ru>
2022-03-15 09:19:11 +0300

    Merge from Origin (conflict resolve)

--- bids/subject/formBidSubjEdit.php
+++ bids/subject/formBidSubjEdit.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 //---------------------------------------------------------------------------------------------------------------------
 //
@@ -47,53 +46,3 @@ $nIdRecord = $_POST["id_record"];      /** @var INTEGER  $nIdRecord */
 
   </form>
 </div>
-=======
-<?php
-//---------------------------------------------------------------------------------------------------------------------
-//
-//  Генерируем HTML-разметку формы редактирования позиции в составе договора
-//  Вызов из: INDEX.JSP
-//
-//---------------------------------------------------------------------------------------------------------------------
-//   @Author: E. Ryabov
-//   @Date:   14.03.2022
-//---------------------------------------------------------------------------------------------------------------------
-
-include("../../db_connect.php");
-
-$cIdUser = $_POST["gnCurrentUserId"];  /** @var INTEGER $cIdUser */
-$nIdRecord = $_POST["id_record"];      /** @var INTEGER  $nIdRecord */
-?>
-
-<div id="pnlSpraEdit">
-  <FORM NAME='frmRecordEdit' ID='frmRecordEdit' role="form" METHOD='POST'>
-    <INPUT ID='txtBidSubjId' NAME='txtBidSubjId' type='hidden' Value='<?php echo $nIdRecord; ?>'>
-
-<?php
-  $sSQL = "Select * FROM bid_subj Where bid_subj.id = ".$nIdRecord." Limit 1";
-
-  $crsBidSubj = mysqli_query($link, $sSQL);
-  if ($rowBidSubj = mysqli_fetch_array($crsBidSubj)){
-    $сNum   = $rowBidSubj['num'];
-    $cName  = htmlspecialchars_decode($rowBidSubj['name']);
-
-    echo '<div class="form-group">';
-    echo '  <div class="col-sm-4"><label class="control-label" for="txtBidSubjNum">Номер:</label></div>';
-    echo '  <div class="col-sm-8">
-              <input class="form-control ctrlFormBidSubj" name="txtBidSubjNum" id="txtBidSubjNum" type="text" value="'.$сNum.'">             
-            </div>';
-    echo '</div>';
-
-    echo '<div class="form-group">';
-    echo '  <div class="col-sm-4"><label class="control-label" for="txtBidSubjName">Назвение:</label></div>';
-    echo '  <div class="col-sm-8">
-              <input class="form-control ctrlFormBidSubj" name="txtBidSubjName" id="txtBidSubjName" type="text" value="'.$cName.'">             
-            </div>';
-    echo '</div>';
-
-  }
-?>
-
-  </form>
-</div>
->>>>>>> ed2c1893eb01616755ba2c46fb5f9eda6325adb1
--- bids/subject/saveBidSubj.php
+++ bids/subject/saveBidSubj.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 //---------------------------------------------------------------------------------------------------------------------
 //
@@ -20,26 +19,3 @@ $sSQL = "Update bid_subj Set num = '".$cNum."', name = '".$cName."' Where id = "
 if (!mysqli_query($link, $sSQL)) echo mysqli_error();
 
 echo $sSQL;
-=======
-<?php
-//---------------------------------------------------------------------------------------------------------------------
-//
-//  Записывает в базу данные в BID_SUBJ
-//
-//---------------------------------------------------------------------------------------------------------------------
-//   Author: E. Ryabov
-//   Date:   14.03.2022
-//---------------------------------------------------------------------------------------------------------------------
-
-include("../../db_connect.php");
-
-$nId   = $_POST["id_record"];
-$cNum = trim(htmlspecialchars($_POST["txtBidSubjNum"], ENT_QUOTES));
-$cName = trim(htmlspecialchars($_POST["txtBidSubjName"], ENT_QUOTES));
-
-$sSQL = "Update bid_subj Set num = '".$cNum."', name = '".$cName."' Where id = ".$nId." Limit 1";
-
-if (!mysqli_query($link, $sSQL)) echo mysqli_error();
-
-echo $sSQL;
->>>>>>> ed2c1893eb01616755ba2c46fb5f9eda6325adb1
